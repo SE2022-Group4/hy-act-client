ec421505b29a8c06101efede3b3d727ca2760784
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-05-15 19:41:47 +0900

    feat: 좌측 Sidebar 구성

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,114 +1,75 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+<template>bind
+  <q-layout view="lHr Lpr lFf">
+    <q-drawer show-if-above bordered side="left">
+      <!-- 네비게이션 링크 -->
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header>한양대학교 비교과 플랫폼</q-item-label>
+        <q-item>비교과 프로그램 신청</q-item>
+        <q-item>비교과 프로그램 출석 확인</q-item>
+        <q-item>수강 이력 및 인증서 발급</q-item>
+        <q-item>마일리지 확인 및 사용</q-item>
       </q-list>
+
+      <div class="fixed-bottom">
+        <q-list>
+          <q-item-label header>지원</q-item-label>
+          <q-item>유지보수팀(031-400-1111)</q-item>
+          <q-item>학사팀(031-400-2222)</q-item>
+          <q-item>전산실(031-400-3333)</q-item>
+        </q-list>
+
+        <div id="language-dialog" style="background-color: #23272B">
+          <q-slide-transition>
+            <q-list
+              v-show="languageListOpened" style="margin-top: 20px">
+              <q-item-label header>언어</q-item-label>
+              <q-item>한국어</q-item>
+              <q-item>English</q-item>
+              <q-item>汉语</q-item>
+            </q-list>
+          </q-slide-transition>
+          <q-btn
+            flat
+            label="언어 설정"
+            :icon-right="languageListOpened ? 'mdi-menu-down' : 'mdi-menu-up'"
+            style="width: 100%;
+          color: #91979b"
+            @click="openLanguageList" />
+        </div>
+      </div>
     </q-drawer>
 
+    <q-drawer show-if-above bordered side="right">
+
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+<style scoped>
+  .q-item__label--header {
+    color: #9994a3;
   }
-];
+  .q-item {
+    color: #91979b;
+  }
+
+  #language-dialog {
+    border-top: solid #5B5D60;
+  }
+</style>
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
   setup () {
-    const leftDrawerOpen = ref(false)
-
+    const languageListOpened = ref(false)
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      languageListOpened,
+      openLanguageList() {
+        languageListOpened.value = !languageListOpened.value
       }
     }
   }
