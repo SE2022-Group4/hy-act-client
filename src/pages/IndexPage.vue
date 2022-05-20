6ce488c7452b23c82fde0679f04d7a1553cdc882
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-05-20 17:33:42 +0900

    feat: 프로그램 항목 추가

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,23 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="column items-center justify-start" style="margin-left: 10px; margin-right: 10px; margin-top: 10px">
+    <ProgramCard v-for="programItem in programItemList" :program="programItem" style="margin-bottom: 10px"/>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+import {defineComponent} from 'vue';
+import ProgramCard from 'components/ProgramCard.vue';
+import {useProgramStore} from 'stores/program.store';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
+  components: { ProgramCard },
   setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200
-    });
-    return { todos, meta };
+    const programStore = useProgramStore();
+    programStore.fetchProgramList();
+    return {
+      programItemList: programStore.programList,
+    };
   }
 });
 </script>
