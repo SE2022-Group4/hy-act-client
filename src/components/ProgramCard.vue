a9363e9b4916ce562e283a302b250b44720be4c3
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-05-23 11:05:11 +0900

    feat: 프로그램을 표시할 Card 추가

---
+++ src/components/ProgramCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-card class="full-width">
+    <q-card-section>
+      <div class="row">
+        <div class="text-h6 col-11" style="text-align: center">
+          {{program.title}}
+        </div>
+        <q-btn icon="mdi-magnify" style="background-color: #343a40; color: #E2E6EA"></q-btn>
+      </div>
+    </q-card-section>
+    <q-separator />
+    <div class="row">
+      <q-card-section class="col-3">
+        <q-img src="icons/favicon-128x128.png" ratio="1" />
+      </q-card-section>
+      <q-card-section class="col-9">
+        <q-list>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">모집 대상</q-item-section>
+            <q-item-section class="col-8">{{program.target_people}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">학년 / 성별</q-item-section>
+            <q-item-section class="col-8">{{program.target_grade}} / {{program.target_gender}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">학과</q-item-section>
+            <q-item-section class="col-8">{{program.target_major}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">마일리지</q-item-section>
+            <q-item-section class="col-8">{{program.mileage}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">날짜</q-item-section>
+            <q-item-section class="col-8">{{targetDate}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">장소</q-item-section>
+            <q-item-section class="col-8">{{program.place}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-4" style="font-weight: bold">상태</q-item-section>
+            <q-item-section class="col-8">{{program.current_people}} / {{program.max_people}}</q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section class="col-12">
+              <q-btn rounded style="background-color: #5B5D60; color: white; padding: 15px">
+                신청하기
+              </q-btn>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+    </div>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'ProgramCard',
+  props: {
+    program: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const zero = num => num < 10 && num >= 0 ? '0' + num : num;
+    const koreanDate = date => `${date.getFullYear()}년 ${zero(date.getMonth() + 1)}월 ${zero(date.getDate())}일 ${zero(date.getHours())}:${zero(date.getMinutes())}`;
+    const targetDate = `${koreanDate(props.program.start_date)} ~ ${koreanDate(props.program.end_date)}`
+    return {targetDate}
+  }
+}
+</script>
+
+<style scoped>
+div .text-h6 {
+  margin: 10px;
+}
+div .q-item__section {
+  text-align: center;
+}
+</style>
