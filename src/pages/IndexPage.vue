53bbe5ab024a2dca7773ab2fa097d5e3fedd41c7
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-05-23 11:38:14 +0900

    feat: 프로그램 검색 창 추가

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,24 @@
 <template>
+  <q-header elevated class="bg-primary">
+    <q-toolbar>
+      <q-toolbar-title>비교과 프로그램 신청</q-toolbar-title>
+      <q-input v-model="searchKeyword" dense>
+        <template v-slot:prepend>
+          <q-icon name="mdi-magnify" />
+        </template>
+      </q-input>
+    </q-toolbar>
+  </q-header>
   <q-page class="column items-center justify-start" style="margin-left: 10px; margin-right: 10px; margin-top: 10px">
-    <ProgramCard v-for="programItem in programItemList" :program="programItem" style="margin-bottom: 10px"/>
+    <ProgramCard
+      v-for="programItem in programItemList"
+      v-show="programItem.title.includes(searchKeyword)"
+      :program="programItem" style="margin-bottom: 10px"/>
   </q-page>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import ProgramCard from 'components/ProgramCard.vue';
 import {useProgramStore} from 'stores/program.store';
 
@@ -17,6 +30,7 @@ export default defineComponent({
     programStore.fetchProgramList();
     return {
       programItemList: programStore.programList,
+      searchKeyword: ref('')
     };
   }
 });
