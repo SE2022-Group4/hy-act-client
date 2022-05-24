e8f1b026f2381d3b3b1a9d1681c59394f82b1641
sohn1029 <sohn1029@gmail.com>
2022-05-24 22:11:37 +0900

    add fixed button - cancel - apply

--- src/pages/Application.vue
+++ src/pages/Application.vue
@@ -3,8 +3,9 @@
     <q-btn label="신청 정보" color="primary" @click="alert = true"></q-btn>
     <q-btn label="자세히 보기" color="primary" @click="detail = true"></q-btn>
 
+    <!--Application Dialog-->
     <q-dialog v-model="alert" transition-show="rotate" transition-hide="rotate">
-      <q-card style="width: 700px; max-width: 80vw">
+      <q-card style="width: 700px; max-width: 100vw">
         <q-card-section>
           <div class="text-h5" style="text-align: center">
             <strong>신청 정보</strong>
@@ -12,7 +13,7 @@
         </q-card-section>
 
         <q-separator color="gray" size="2px" inset></q-separator>
-        <q-card-section>
+        <q-card-section style="padding: 0px; max-height: 65vh" class="scroll">
           <div class="row">
             <!--Program Image-->
             <div class="col-6">
@@ -61,11 +62,9 @@
               </p>
             </div>
           </div>
-        </q-card-section>
 
-        <q-separator color="gray" size="2px" inset></q-separator>
+          <q-separator color="gray" size="2px" inset></q-separator>
 
-        <q-card-section class="q-pt-none">
           <!--Applicant Info-->
           <div class="row">
             <div class="col">
@@ -130,23 +129,26 @@
               <p style="text-align: center; line-height: 20px">
                 - 참여 완료 시 마일리지 60포인트가 제공됩니다.
               </p>
-
-              <q-btn
-                class="items-center"
-                unelevated
-                rounded
-                color="grey-9"
-                label="신청하기"
-              ></q-btn>
             </div>
           </div>
         </q-card-section>
-
+        <q-separator inset></q-separator>
         <q-card-actions align="right">
+          <div class="row" style="padding: 0px">
+            <q-btn
+              class="items-center"
+              unelevated
+              rounded
+              color="grey-9"
+              label="&nbsp;&nbsp;신청하기&nbsp;&nbsp;"
+            ></q-btn>
+          </div>
           <q-btn flat label="닫기" color="primary" v-close-popup></q-btn>
         </q-card-actions>
       </q-card>
     </q-dialog>
+
+    <!--Detail Dialog-->
     <q-dialog
       v-model="detail"
       transition-show="rotate"
@@ -158,9 +160,78 @@
             <strong>세부 정보</strong>
           </div>
         </q-card-section>
-
         <q-separator color="gray" size="2px" inset></q-separator>
+        <q-card-section style="max-height: 60vh; padding: 0px" class="scroll">
+          <div class="row">
+            <div class="col-2">
+              <p style="font-size: 1em; margin: 0px">
+                <strong> 일시 </strong>
+              </p>
+            </div>
+            <div class="col-10" style="font-size: 1em">
+              <p style="margin: 0px">2022. 5. 27(금) 16:00 ~ 19:00</p>
+            </div>
+          </div>
 
+          <q-separator color="gray" size="2px" inset></q-separator>
+
+          <div class="row">
+            <div class="col-2">
+              <p style="font-size: 1em; margin: 0px">
+                <strong> 장소 </strong>
+              </p>
+            </div>
+            <div class="col-10" style="font-size: 1em">
+              <p style="margin: 0px">백남학술정보관 국제회의실(6층)</p>
+            </div>
+          </div>
+
+          <q-separator color="gray" size="2px" inset></q-separator>
+          <div class="row">
+            <div class="col-2">
+              <p style="font-size: 1em; margin: 0px">
+                <strong> 신청기간 </strong>
+              </p>
+            </div>
+            <div class="col-10" style="font-size: 1em">
+              <p style="margin: 0px">2022. 3.30(수) ~ 4.15(금) 16:00</p>
+            </div>
+          </div>
+
+          <q-separator color="gray" size="2px" inset></q-separator>
+
+          <div class="row">
+            <div class="col-2">
+              <p style="font-size: 1em; margin: 0px">
+                <strong> 신청대상 </strong>
+              </p>
+            </div>
+            <div class="col-10" style="font-size: 1em">
+              <p style="margin: 0px">
+                한양대학교 서울 학부생(전년도 우승팀 제외)<br />
+                * 단, 휴학생 신청 불가(사유 : 장학금으로 상금 지급)<br />
+                * 1인도 신청가능(주최 측에서 팀 매칭 예정)
+              </p>
+            </div>
+          </div>
+
+          <q-separator color="gray" size="2px" inset></q-separator>
+
+          <div class="row">
+            <div class="col-2">
+              <p style="font-size: 1em; margin: 0px">
+                <strong> 내용 </strong>
+              </p>
+            </div>
+            <div class="col-10" style="font-size: 1em">
+              <p style="margin: 0px">
+                지정도서로 선정된 도서를 읽고 하브루타 방식으로 진행하는
+                토론대회
+              </p>
+            </div>
+          </div>
+        </q-card-section>
+        <q-separator inset></q-separator>
         <q-card-actions align="right">
           <q-btn flat label="닫기" color="primary" v-close-popup></q-btn>
         </q-card-actions>
