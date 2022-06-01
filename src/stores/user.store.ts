d0789e1a8fc7c2a7d0e1a3b088c37f4786811e97
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-06-01 19:46:01 +0900

    fix: User 초기화 코드 변경

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -9,12 +9,20 @@ export type UserState = {
 export const useUserStore = defineStore({
   id: 'userStore',
   state: () => ({user: {
-    name: '', email: '', grade: 0, major: '', mileage: 0, reserved_programs: [],
+    name: '', email: '', grade: 0, group: '', major: '', mileage: 0, reserved_programs: [],
   }} as UserState),
   actions: {
     async fetchUser() {
       const response = await api.get('/user');
       this.user = response.data;
+    },
+    async logout() {
+      await api.post('/logout');
+      this.user = {
+        name: '', email: '', grade: 0, group: '', major: '', mileage: 0, reserved_programs: [],
+      } as UserItem;
+      localStorage.removeItem('refreshToken');
+      localStorage.removeItem('accessToken');
     }
   }
 })
