b9f7a9944c14af4cdd224d4ff775b2361b226be4
Jeongin Lee <jil8885@hanyang.ac.kr>
2022-06-04 18:21:09 +0900

    feat: 사용자 정보 요청 실패시 로그인 페이지로 이동

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -4,17 +4,23 @@ import {api} from 'boot/axios';
 
 export type UserState = {
   user: UserItem;
+  success: boolean;
 }
 
 export const useUserStore = defineStore({
   id: 'userStore',
   state: () => ({user: {
     name: '', email: '', grade: 0, group: '', major: '', mileage: 0, reserved_programs: [],
-  }} as UserState),
+  }, success: true} as UserState),
   actions: {
-    async fetchUser() {
-      const response = await api.get('/user');
-      this.user = response.data;
+    fetchUser() {
+      api.get('/user/info').then(res => {
+        this.user = res.data;
+      }).catch((error) => {
+        console.log(error);
+        this.success = false;
+        window.location.href = '/login'
+      });
     },
     async logout() {
       await api.post('/logout');
